b41cf729418b0e01d753f60ca5a073108458e161
Iain MacDonald <IJMacD@gmail.com>
2021-05-14 19:34:16 +0800

    Treat input as one whole sequence

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import { findAssignment } from './address';
 function App() {
   const [ input, setInput ] = useState("");
 
-  const ipList = [...input].map(c => [...utf8.encode(c).padStart(4, "\0")].map(b => b.charCodeAt(0)));
+  const utf8Bytes = utf8.encode(input);
+
+  let ipList = input.length > 0 ? utf8Bytes.match(/.{1,4}/g).map(s => [...s.padEnd(4,"\0")].map(c => c.charCodeAt(0))) : [];
 
   return (
     <div className="App">
       <input className="EmojiInput" value={input} onChange={e => setInput(e.target.value)} placeholder="Enter some characters" />
+      <p className="ByteList">{[...utf8Bytes].map(c => c.charCodeAt(0).toString(16).padStart(2, "0")).join(" ")}</p>
       {
         ipList.map((ip, i) => {
           const ipv4 = `${ip[0]}.${ip[1]}.${ip[2]}.${ip[3]}`;
